e797d0de56d9a2888cc595348884a4713c24869f
Fredy Palma <fpalma@siccob.com.mx>
2018-03-13 18:23:15 -0600

    Seguiendo con el proyecto final de HTML

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 const pug = require('gulp-pug');
 const rename = require('gulp-rename');
 const browserSync = require('browser-sync');
@@ -15,11 +17,21 @@ gulp.task('pug', () => {
             .pipe(gulp.dest('./public/'));
 });
 
+gulp.task('sass', () => {
+    gulp.src('./dev/scss/styles.scss')
+            .pipe(sass({
+                outputStyle: 'compressed'
+            }))
+            .pipe(gulp.dest('./public/css'))
+            .pipe(server.stream({match: '**/*.css'}));
+});
+
 gulp.task('default', () => {
     server.init({
         proxy: 'http://localhost/PruebasHtml/public/index.php'
     });
     gulp.watch('./dev/pug/**/*.pug', ['pug', server.reload]);
+    gulp.watch('./dev/scss/**/*.scss', ['sass']);
 });
 
 
--- public/index.php
+++ public/index.php
@@ -4,16 +4,37 @@
     <title>Inicio | Creativos ABC</title>
     <meta charset="utf-8">
     <meta name="description" content="Bienvenidos al sitio web Creativos ABC">
+    <link rel="stylesheet" href="https://rawgit.com/escueladigital/EDgrid/master/css/ed-grid.min.css">
+    <link rel="stylesheet" href="/PruebasHtml/public/css/styles.css">
   </head>
   <body>
-    <header><img src="/PruebasHtml/public/img/logo.svg">
-      <ul>
-        <li><a href="/PruebasHtml/public/">home</a></li>
-        <li><a href="nosotros.php">nosotros</a></li>
-        <li><a href="servicios.php">servicios</a></li>
-        <li><a href="portafolio.php">portafolio</a></li>
-        <li><a href="contacto.php">contacto</a></li>
-      </ul>
+    <header class="main-header"><a href="/PruebasHtml/public/"><img class="logo" src="/PruebasHtml/public/img/logo.svg"></a>
+      <nav class="menu"> 
+        <ul>
+          <li><a href="/PruebasHtml/public/">home</a></li>
+          <li><a href="nosotros.php">nosotros</a></li>
+          <li><a href="servicios.php">servicios</a></li>
+          <li><a href="portafolio.php">portafolio</a></li>
+          <li><a href="contacto.php">contacto</a></li>
+        </ul>
+      </nav>
     </header>
+    <div><img src="/PruebasHtml/public/img/inicio.jpg">
+    </div>
+    <main>
+      <h1>Bienvenidos</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.      
+      </p>
+    </main>
+    <aside>
+    </aside>
+    <footer>
+      <p>Creativos ABC - Todos los derechos reservados</p>
+    </footer>
   </body>
 </html>
